2125934287dc95ce90c498bbcd3f86ef7b4dcac1
olu <adeoyeseunengineer@hotmail.com>
2023-12-19 19:07:54 -0600

    testing new app service

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ourfirst_appservice_code.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
